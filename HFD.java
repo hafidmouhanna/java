3ccecf3cab39ecab7fcccf3ab3a293da4f0c2de8
hafidmouhanna <79752425+hafidmouhanna@users.noreply.github.com>
2021-02-27 14:34:37 +0100

    Update HFD.java

--- HFD.java
+++ HFD.java
@@ -1,8 +1,10 @@
-
-public class HFD {
-    
+//calculer la suite de 1 au 100
+public class HFD {   
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    int som;
+    For(int i=1;i<=100;i++){
+    som+=i;}
+System.out.println("la somme de la suite est egal a " +som);
     }
     
 }
